e56294d1a67d050fa9d6358e69625d50474f3a27
neviton viana <nevitonviana1hotmail.com>
2022-10-22 20:41:26 -0300

    criação dos model service repository de busca das tasks no banco de dados

--- lib/repositories/tasks/tasks_repository_impl.dart
+++ lib/repositories/tasks/tasks_repository_impl.dart
@@ -1,4 +1,5 @@
 import '../../core/database/sqlite_connection_factory.dart';
+import '../../models/task_model.dart';
 import 'tasks_repository.dart';
 
 class TasksRepositoryImpl implements TasksRepository {
@@ -17,5 +18,19 @@ class TasksRepositoryImpl implements TasksRepository {
       'data_hora': date.toIso8601String(),
       'finalizado': 0
     });
+  }
+
+  @override
+  Future<List<TaskModel>> findByPeriod(DateTime star, DateTime end) async {
+    final starFilter = DateTime(star.year, star.month, star.day, 0, 0, 0);
+    final endFilter = DateTime(end.year, end.month, end.day, 23, 59, 59);
+    final conn = await _sqliteConnectionFactory.openConnection();
+    final result = await conn.rawQuery('''
+    select * from todo
+    where data_hora ? and ?
+    order by data_hora
+    ''', [starFilter.toIso8601String(), endFilter.toIso8601String()]);
+
+    return result.map((e) => TaskModel.loadFromDB(e)).toList();
   }
 }
--- lib/services/tasks/tasks_service_impl.dart
+++ lib/services/tasks/tasks_service_impl.dart
@@ -1,3 +1,6 @@
+import 'package:todo_list/models/task_model.dart';
+
+import '../../models/week_task_model.dart';
 import '../../repositories/tasks/tasks_repository.dart';
 import 'tasks_service.dart';
 
@@ -10,4 +13,33 @@ class TasksServiceImpl extends TasksService {
   @override
   Future<void> save(DateTime date, String description) =>
       _tasksRepository.save(date, description);
+
+  @override
+  Future<List<TaskModel>> getToday() {
+    return _tasksRepository.findByPeriod(DateTime.now(), DateTime.now());
+  }
+
+  @override
+  Future<List<TaskModel>> getTomorrow() {
+    var tomorrowDate = DateTime.now().add(const Duration(days: 1));
+    return _tasksRepository.findByPeriod(tomorrowDate, tomorrowDate);
+  }
+
+  @override
+  Future<WeekTaskModel> getWeek() async {
+    final today = DateTime.now();
+    var starFilter = DateTime(today.year, today.month, today.day, 0, 0, 0);
+    DateTime endFilter;
+
+    if (starFilter.weekday != DateTime.monday) {
+      starFilter =
+          starFilter.subtract(Duration(days: (starFilter.weekday - 1)));
+    }
+
+    endFilter = starFilter.add(const Duration(days: 7));
+    final tasks = await _tasksRepository.findByPeriod(starFilter, endFilter);
+
+    return WeekTaskModel(
+        startDate: starFilter, endDate: endFilter, tasks: tasks);
+  }
 }
